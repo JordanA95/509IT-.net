88c88497f288c998d71968b813c6775e654b2dc0
JordanA95 <47454658+JordanA95@users.noreply.github.com>
2021-06-22 19:32:07 +0100

    .net GitHub 22.6.21

---
+++ .net 22.6.21v1.5/Harris.net/Harris.net/Models/PersonalContact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Harris.net.Models
+{
+    public class PersonalContact
+    {
+        public int PersonalContactID { get; set; }
+        public string PersonalContactFName { get; set; }
+        public string PersonalContactLName { get; set; }
+        public string PersonalContactEmail { get; set; }
+        public string PersonalTel { get; set; }
+        public string PersonalContactAddr1 { get; set; }
+        public string PersonalContactAddr2 { get; set; }
+        public string PersonalContactCity { get; set; }
+        public string PersonalContactPostcode { get; set; }
+
+    }
+}
